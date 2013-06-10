e0241629ef7248cde60174f37824d1fa3fb4f870
Joe Thornber <ejt@redhat.com>
2013-06-10 12:04:17 +0100

    remove the hard coded 'reports/' from TestOutcome

--- lib/dmtest/test-outcome.rb
+++ lib/dmtest/test-outcome.rb
@@ -5,12 +5,12 @@ def mangle(txt)
 end
 
 class TestOutcome
-  attr_accessor :suite, :name, :log_path, :time
+  attr_accessor :suite, :name, :log_file, :time
 
   def initialize(s, n, t = nil)
     @suite = s
     @name = n
-    @log_path = "reports/#{mangle(s)}_#{mangle(n)}.log"
+    @log_file = "#{mangle(s)}_#{mangle(n)}.log"
     @time = t || Time.now
     @pass = true
   end
