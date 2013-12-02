be5fe31e59e07bc1d9f84472a88ac94ddeb35514
Joe Thornber <ejt@redhat.com>
2013-12-02 11:57:42 +0000

    [cache] refactor ResizeTests

--- lib/dmtest/tests/cache/resize_tests.rb
+++ lib/dmtest/tests/cache/resize_tests.rb
@@ -26,6 +26,27 @@ class ResizeTests < ThinpTestCase
     @cache_blocks = 1024
   end
 
+  def setup_metadata(nr_blocks)
+    s = make_stack(:format => false,
+                   :cache_blocks => nr_blocks)
+    s.activate_support_devs do
+      s.prepare_populated_cache
+      md = dump_metadata(s.md)
+      md.superblock.nr_cache_blocks.should == nr_blocks
+      md
+    end
+  end
+
+  def activate_kernel(nr_blocks)
+    s = make_stack(:format => false,
+                   :cache_blocks => nr_blocks,
+                   :policy => Policy.new(:mq, :migration_threshold => 0))
+    s.activate_support_devs do
+      s.activate_top_level {}
+      dump_metadata(s.md)
+    end
+  end
+
   #--------------------------------
 
   def test_no_resize_retains_mappings_all_clean
@@ -64,36 +85,32 @@ class ResizeTests < ThinpTestCase
     end
   end
 
+  #--------------------------------
+
+  def grow_test(nr_blocks)
+    md1 = setup_metadata(nr_blocks)
+    new_nr_blocks = nr_blocks + 5678
+    md2 = activate_kernel(new_nr_blocks)
+    md2.mappings.should == md1.mappings
+    md2.superblock.nr_cache_blocks.should == new_nr_blocks
+  end
+
   def test_metadata_can_grow
-    [23, 513, 1023, 4095].each do |nr_blocks|
-      s = make_stack(:format => false,
-                     :cache_blocks => nr_blocks + 5678)
-      s.activate_support_devs do
-        s.prepare_populated_cache
-        md1 = dump_metadata(s.md)
+    [23, 513, 1023, 4095].each {|nr_blocks| grow_test(nr_blocks)}
+  end
 
-        s.activate_top_level {}
+  #--------------------------------
 
-        md2 = dump_metadata(s.md)
-        assert_equal(md1.mappings, md2.mappings)
-      end
-    end
+  def shrink_test(nr_blocks)
+    md1 = setup_metadata(nr_blocks)
+    new_nr_blocks = nr_blocks / 2
+    md2 = activate_kernel(new_nr_blocks)
+    md2.mappings.should == md1.mappings[0..new_nr_blocks - 1]
+    md2.superblock.nr_cache_blocks.should == new_nr_blocks
   end
 
   def test_metadata_can_shrink
-    [23, 345, 513, 876, 1023, 2345, 4095].each do |nr_blocks|
-      s = make_stack(:format => false,
-                     :cache_blocks => nr_blocks / 2)
-      s.activate_support_devs do
-        s.prepare_populated_cache
-        md1 = dump_metadata(s.md)
-
-        s.activate_top_level {}
-
-        md2 = dump_metadata(s.md)
-        assert_equal(md1.mappings[0..nr_blocks / 2 - 1], md2.mappings)
-      end
-    end
+    [23, 345, 513, 876, 1023, 2345, 4095].each {|nr_blocks| shrink_test(nr_blocks)}
   end
 end
 
