138f320e9bd3c4cd9f935a7dca67c4d2b7ac12ac
Joe Thornber <ejt@redhat.com>
2014-01-20 14:29:42 +0000

    [thin-provisioning] throughput tests that use over provisioning

--- lib/dmtest/tests/thin-provisioning/throughput_tests.rb
+++ lib/dmtest/tests/thin-provisioning/throughput_tests.rb
@@ -34,12 +34,11 @@ class ThroughputTests < ThinpTestCase
     end
   end
 
-  def across_various_bpa_block_and_io_sizes(&block)
-    [1, 10, 20].each do |blocks_per_allocation|
-      across_various_block_and_io_sizes do |block_size, io_size|
-        block.call(block_size, io_size, blocks_per_allocation)
+  def across_various_bpa_and_io_sizes(&block)
+    [1, 16, 64, 256, 1024].each do |bpa|
+      across_various_io_sizes do |io_size|
+        block.call(bpa, io_size)
       end
-      STDERR.puts "\n"
     end
   end
 
@@ -122,7 +121,7 @@ class ThroughputTests < ThinpTestCase
   def test_snap_breaking_throughput
     across_various_block_and_io_sizes do |block_size, io_size|
       throughput_snapped(block_size, io_size)
-    end      
+    end
   end
 
   def test_snap_already_broken_throughput
@@ -137,36 +136,77 @@ class ThroughputTests < ThinpTestCase
     end
   end
 
-  def multithreaded_layout_reread(device, block_size, io_size, bpa)
+  #----------------------------------------------------------------
+
+  extend DiskUnits
+  VOLUME_SIZE = meg(256)
+
+  def iozone_then_dd_read(device, io_size)
+    count = (dev_size(device) / 9) / meg(1)
+
+
     # Use iozone to layout interleaved files on device and then re-read with dd using DIO
     fs = FS::file_system(:xfs, device)
     fs.format
     fs.with_mount("./mnt1") do
-
-      report_time("iozone init block_size = #{block_size}, io_size = #{io_size}, blocks_per_allocation = #{bpa}", STDERR) do
-        ProcessControl.run("iozone -i 0 -i 1 -w -+n -+N -c -C -e -s 768m -r #{io_size / 2}k -t 8 -F ./mnt1/1 ./mnt1/2 ./mnt1/3 ./mnt1/4 ./mnt1/5 ./mnt1/6 ./mnt1/7 ./mnt1/8")
+      report_time("iozone init, io_size = #{io_size}", STDERR) do
+        ProcessControl.run("iozone -i 0 -i 1 -w -+n -+N -c -C -e -s #{count}m -r #{io_size / 2}k -t 8 -F ./mnt1/1 ./mnt1/2 ./mnt1/3 ./mnt1/4 ./mnt1/5 ./mnt1/6 ./mnt1/7 ./mnt1/8")
       end
 
       ProcessControl.run('echo 3 > /proc/sys/vm/drop_caches')
 
-      report_time(" dd re-read  block_size = #{block_size}, io_size = #{io_size}, blocks_per_allocation = #{bpa}", STDERR) do
+      report_time("dd re-read, io_size = #{io_size}", STDERR) do
         ProcessControl.run("dd iflag=direct if=./mnt1/1 of=/dev/null bs=#{io_size / 2}k")
+        ProcessControl.run("dd iflag=direct if=./mnt1/2 of=/dev/null bs=#{io_size / 2}k")
+        ProcessControl.run("dd iflag=direct if=./mnt1/3 of=/dev/null bs=#{io_size / 2}k")
+        ProcessControl.run("dd iflag=direct if=./mnt1/4 of=/dev/null bs=#{io_size / 2}k")
       end
     end
   end
 
-  def multithreaded_layout_read(device, block_size, io_size, bpa)
-    report_time("dd write  block_size = #{block_size}, io_size = #{io_size}, blocks_per_allocation = #{bpa}", STDERR) do
-      in_parallel(0, 1, 2, 3) {|offset| ProcessControl.run("dd oflag=direct if=/dev/zero of=#{device} bs=#{io_size * 512} count=#{gig(1) / io_size} seek=#{offset * (gig(1) / io_size)}")}
+  def multi_write_single_read(device, io_size)
+    report_time("dd write, io_size = #{io_size}", STDERR) do
+      in_parallel(0, 1, 2, 3) do |thread|
+        count = (dev_size(device) / 4) / io_size
+        offset = count * thread
+        ProcessControl.run("dd oflag=direct if=/dev/zero of=#{device} bs=#{io_size * 512} count=#{count} seek=#{offset}")
+      end
     end
 
-    report_time(" dd read  block_size = #{block_size}, io_size = #{io_size}, blocks_per_allocation = #{bpa}", STDERR) do
-      ProcessControl.run("dd iflag=direct if=#{device} of=/dev/null bs=#{io_size * 512} count=#{gig(1) / io_size}")
+    # FIXME: shouldn't have an effect
+    ProcessControl.run('echo 3 > /proc/sys/vm/drop_caches')
+
+    report_time("dd read, io_size = #{io_size}", STDERR) do
+      ProcessControl.run("dd iflag=direct if=#{device} of=/dev/null bs=#{io_size * 512}")
     end
   end
 
-  def throughput_multithreaded_layout_io(block_size, io_size, bpa, desc, &block)
-    @volume_size = gig(7)
+  def with_bpa_volume(block_size, bpa, desc, &block)
+    @volume_size = VOLUME_SIZE
+
+    @blocks_per_dev = div_up(@volume_size, block_size * bpa)
+    @volume_size = @blocks_per_dev * block_size * bpa
+    @size = @volume_size
+
+    # FIXME: add a :format option to with_standard_pool
+    wipe_device(@metadata_dev, 8)
+
+    with_standard_pool(@size, :zero => false, :block_size => block_size,
+                       :blocks_per_allocation => bpa) do |pool|
+      with_new_thin(pool, @volume_size, 0, &block)
+
+      pool.pause do
+        dump_metadata(@metadata_dev) do |xml_path|
+          file = "/tmp/metadata_#{desc}.xml"
+          ProcessControl.run("cp #{xml_path} #{file}")
+          STDERR.puts "metadata dumped to #{file}"
+        end
+      end
+    end
+  end
+
+  def with_bpa_snap_volume(block_size, bpa, desc, &block)
+    @volume_size = VOLUME_SIZE
 
     @blocks_per_dev = div_up(@volume_size, block_size * bpa)
     @volume_size = @blocks_per_dev * block_size * bpa
@@ -178,58 +218,70 @@ class ThroughputTests < ThinpTestCase
     with_standard_pool(@size, :zero => false, :block_size => block_size,
                        :blocks_per_allocation => bpa) do |pool|
       with_new_thin(pool, @volume_size, 0) do |thin|
-        block.call(thin, block_size, io_size, bpa)
+        wipe_device(thin)
+
+        thin.pause do
+          pool.message(0, "create_snap 1 0")
+        end
+
+        block.call(thin)
       end
 
-      pool.suspend
-      dump_metadata(@metadata_dev) do |xml_path|
-        # FIXME: log to unique location
-        ProcessControl.run("cat #{xml_path} > /tmp/testest/#{desc}_bs#{block_size}_io#{io_size}_bpa#{bpa}.txt")
-      end
-      pool.resume
-    end
-  end
-
-  def throughput_multithreaded_layout_io_linear(block_size, io_size, &block)
-    with_standard_linear(:data_size => gig(7)) do |linear|
-      block.call(linear, block_size, io_size, 1)
-    end
-  end
-
-  def harness_multithreaded_layout_io_throughput(&block)
-    # compare: linear vs blocks_per_allocation=1
-    STDERR.puts "\nlinear:"
-    across_various_block_and_io_sizes do |block_size, io_size|
-      throughput_multithreaded_layout_io_linear(block_size, io_size, &block)
-    end
-
-    # then compare: use of blocks_per_allocation=10
-    STDERR.puts "\nthin w/ varied blocks_per_allocation:"
-    across_various_bpa_block_and_io_sizes do |block_size, io_size, bpa|
-      throughput_multithreaded_layout_io(block_size, io_size, bpa, "thin_varied_bpa", &block)
-    end
-
-    # versus: native large blocksizes
-    STDERR.puts "\nthin w/ native large blocksizes:"
-    [k(640), k(1280), k(2560), k(5120), k(10240)].each do |block_size|
-      across_various_io_sizes do |io_size|
-        throughput_multithreaded_layout_io(block_size, io_size, 1, "thin_large_bs", &block)
+      pool.pause do
+        dump_metadata(@metadata_dev) do |xml_path|
+          file = "/tmp/metadata_#{desc}.xml"
+          ProcessControl.run("cp #{xml_path} #{file}")
+          STDERR.puts "metadata dumped to #{file}"
+        end
       end
     end
   end
 
-  def test_multithreaded_layout_reread_throughput
-    harness_multithreaded_layout_io_throughput do |device, block_size, io_size, bpa|
-      multithreaded_layout_reread(device, block_size, io_size, bpa)
+  def test_multiple_writers_then_single_reader_linear
+    across_various_io_sizes do |io_size|
+      with_standard_linear(:data_size => VOLUME_SIZE) do |linear|
+        multi_write_single_read(linear, io_size)
+      end
     end
   end
 
-  def test_multithreaded_layout_read_throughput
-    harness_multithreaded_layout_io_throughput do |device, block_size, io_size, bpa|
-      multithreaded_layout_read(device, block_size, io_size, bpa)
+  def test_multiple_writers_then_single_reader_thin_various_bpa
+    block_size = k(64)
+    across_various_bpa_and_io_sizes do |bpa, io_size|
+      STDERR.puts "bpa = #{bpa}, io_size = #{io_size}"
+      with_bpa_volume(block_size, bpa, "mw_sr_bpa_#{bpa}_io_size_#{io_size}") do |thin|
+        multi_write_single_read(thin, io_size)
+      end
     end
   end
 
+  def test_multiple_writers_then_single_reader_snap_various_bpa
+    block_size = k(64)
+    across_various_bpa_and_io_sizes do |bpa, io_size|
+      STDERR.puts "bpa = #{bpa}, io_size = #{io_size}"
+      with_bpa_snap_volume(block_size, bpa, "mw_sr_bpa_#{bpa}_io_size_#{io_size}") do |thin|
+        multi_write_single_read(thin, io_size)
+      end
+    end
+  end
+
+  def test_iozone_writer_sequential_reader_linear
+    across_various_io_sizes do |io_size|
+      with_standard_linear(:data_size => VOLUME_SIZE) do |linear|
+        iozone_then_dd_read(linear, io_size)
+      end
+    end
+  end
+
+  def test_iozone_writer_sequential_reader_thin_various_bpa
+    block_size = k(64)
+    across_various_bpa_and_io_sizes do |bpa, io_size|
+      STDERR.puts "bpa = #{bpa}, io_size = #{io_size}"
+      with_bpa_volume(block_size, bpa, "mw_sr_bpa_#{bpa}_io_size_#{io_size}") do |thin|
+        iozone_then_dd_read(thin, io_size)
+      end
+    end
+  end
 end
 
 #----------------------------------------------------------------
