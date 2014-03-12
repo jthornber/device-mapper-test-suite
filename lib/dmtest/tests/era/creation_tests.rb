6bc32d687f63a94b6df6204c8024dc4f191f6c94
Joe Thornber <ejt@redhat.com>
2014-03-12 14:48:59 +0000

    [era] Tests for the largest valid origin

--- lib/dmtest/tests/era/creation_tests.rb
+++ lib/dmtest/tests/era/creation_tests.rb
@@ -20,6 +20,33 @@ class CreationTests < ThinpTestCase
     s = EraStack.new(@dm, @metadata_dev, @data_dev, :format => true)
     s.activate {}
   end
+
+  def test_largest_valid
+    biggest = 137438953408      # 2^31 - 1 * 32k blocks
+
+    table = Table.new(ErrorTarget.new(biggest))
+
+    with_dev(table) do |fake_data|
+      s = EraStack.new(@dm, @metadata_dev, fake_data,
+                       :block_size => 64,
+                       :format => true)
+
+      s.activate {}
+    end
+  end
+
+  def test_smallest_invalid
+    too_big = 137438953472      # 2^31 * 32k blocks
+
+    failed = false
+    begin
+      s.activate {}
+    rescue
+      failed = true
+    end
+
+    failed.should be_true
+  end
 end
 
 #----------------------------------------------------------------
