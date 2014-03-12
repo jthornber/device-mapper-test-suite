f0439a1316940bedb06ba2de8b4df072eac400eb
Joe Thornber <ejt@redhat.com>
2014-03-12 15:32:05 +0000

    update origin size tests

--- lib/dmtest/tests/era/creation_tests.rb
+++ lib/dmtest/tests/era/creation_tests.rb
@@ -21,13 +21,14 @@ class CreationTests < ThinpTestCase
     s.activate {}
   end
 
-  def test_largest_valid
+  # This fails because we don't have enough metadata space
+  def _test_largest_valid_origin
     biggest = 137438953408      # 2^31 - 1 * 32k blocks
 
     table = Table.new(ErrorTarget.new(biggest))
-
     with_dev(table) do |fake_data|
-      s = EraStack.new(@dm, @metadata_dev, fake_data,
+      s = EraStack.new(@dm, @data_dev, fake_data,
+                       :origin_size => biggest,
                        :block_size => 64,
                        :format => true)
 
@@ -35,17 +36,25 @@ class CreationTests < ThinpTestCase
     end
   end
 
-  def test_smallest_invalid
+  def test_smallest_invalid_origin
     too_big = 137438953472      # 2^31 * 32k blocks
 
-    failed = false
-    begin
-      s.activate {}
-    rescue
-      failed = true
-    end
+    table = Table.new(ErrorTarget.new(too_big))
+    with_dev(table) do |fake_data|
+      s = EraStack.new(@dm, @metadata_dev, fake_data,
+                       :origin_size => too_big,
+                       :block_size => 64,
+                       :format => true)
 
-    failed.should be_true
+      failed = false
+      begin
+        s.activate {}
+      rescue
+        failed = true
+      end
+
+      failed.should be_true
+    end
   end
 end
 
