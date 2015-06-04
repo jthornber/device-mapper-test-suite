32b01c42ce007d55603dd7f39b99e7b0fca83bec
Joe Thornber <ejt@redhat.com>
2015-06-04 15:55:12 +0100

    cache-good work

--- lib/dmtest/tests/cache/metadata_scaling_tests.rb
+++ lib/dmtest/tests/cache/metadata_scaling_tests.rb
@@ -25,6 +25,8 @@ class MetadataScalingTests < ThinpTestCase
   include CacheUtils
   extend TestUtils
 
+  POLICY_NAMES = %w(mq smq)
+
   def setup
     super
     @data_block_size = k(32)
@@ -37,13 +39,13 @@ class MetadataScalingTests < ThinpTestCase
 
   def test_metadata_use_restored
     data_size = gig(1)
-    block_size = k(32)
-    data_blocks = data_size / block_size
+    block_size = k(64)
 
     [[1024, 12], [2048, 15], [4096, 21], [8192, 33], [16384, 57]].each do |cache_blocks, expected_metadata_use|
-      s = make_stack(:data_size => gig(4),
-                     :block_size => k(32),
-                     :cache_blocks => cache_blocks)
+      s = CacheStack.new(@dm, @metadata_dev, @data_dev,
+                         :data_size => gig(4),
+                         :block_size => block_size,
+                         :cache_size => cache_blocks * block_size)
       s.activate_support_devs do
         s.prepare_populated_cache(:dirty_percentage => 100)
         s.activate_top_level do
@@ -56,33 +58,29 @@ class MetadataScalingTests < ThinpTestCase
     end
   end
 
-  def test_metadata_use_kernel
-    big = 10240000
+  def metadata_use_kernel(policy)
     data_size = gig(1)
-    block_size = k(32)
+    block_size = k(64)
     data_blocks = data_size / block_size
 
     [[1024, 9], [2048, 11], [4096, 15], [8192, 23], [16384, 39]].each do |cache_blocks, expected_metadata_use|
-      s = make_stack(:data_size => gig(4),
-                     :block_size => k(32),
-                     :cache_blocks => cache_blocks,
-                     :policy => Policy.new('mq',
-                                           :migration_threshold => big,
-                                           :sequential_threshold => big,
-                                           :read_promote_adjustment => 0,
-                                           :write_promote_adjustment => 0,
-                                           :discard_promote_adjustment => 0))
+      s = CacheStack.new(@dm, @metadata_dev, @data_dev,
+                         :data_size => gig(4),
+                         :block_size => block_size,
+                         :cache_size => cache_blocks * block_size,
+                         :policy => Policy.new(policy, :migration_threshold => 1024000))
       s.activate do
         wipe_device(s.cache)
 
         status = CacheStatus.new(s.cache)
-        status.residency.should == cache_blocks
         status.md_used.should == expected_metadata_use
 
         STDERR.puts "cache_blocks #{cache_blocks}: #{status.md_used}/#{status.md_total}"
       end
     end
   end
+
+  define_tests_across(:metadata_use_kernel, POLICY_NAMES)
 end
 
 #----------------------------------------------------------------
