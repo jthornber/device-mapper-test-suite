20eb8405a2e5c3c659e08f7f48a2b9791e512c95
Joe Thornber <ejt@redhat.com>
2015-08-21 09:42:58 +0100

    [cache] reintroduce small and large tests.
    These shouldn't have been removed.

---
+++ lib/dmtest/tests/cache/small_cache_tests.rb
@@ -0,0 +1,51 @@
+require 'dmtest/cache-status'
+require 'dmtest/disk-units'
+require 'dmtest/git'
+require 'dmtest/log'
+require 'dmtest/pool-stack'
+require 'dmtest/test-utils'
+require 'dmtest/cache_stack'
+require 'dmtest/cache_utils'
+require 'dmtest/cache_policy'
+require 'dmtest/thinp-test'
+require 'dmtest/tvm.rb'
+require 'dmtest/utils'
+
+require 'rspec/expectations'
+
+#----------------------------------------------------------------
+
+class SmallConfigTests < ThinpTestCase
+  include Utils
+  include DiskUnits
+  include DMThinUtils
+  include CacheUtils
+  extend TestUtils
+
+  POLICY_NAMES = %w(mq smq)
+
+  def setup
+    super
+    @data_block_size = k(32)
+    @cache_blocks = 1024
+  end
+
+  #--------------------------------
+
+  def small_stack(policy)
+    s = CacheStack.new(@dm, @metadata_dev, @data_dev,
+                       :format => true,
+                       :metadata_size => meg(8),
+                       :block_size => k(32),
+                       :cache_size => k(50),
+                       :data_size => k(50),
+                       :policy => Policy.new(policy, :migration_threshold => 1024))
+    s.activate do
+      wipe_device(s.cache)
+    end
+  end
+
+  define_tests_across(:small_stack, POLICY_NAMES)
+end
+
+#----------------------------------------------------------------
