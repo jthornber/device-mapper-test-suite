d46300f61bae34f242fe63a7a77ccb343d86c1d5
Joe Thornber <ejt@redhat.com>
2015-12-04 13:58:42 +0000

    [thin] Add thin_dump_a_metadata_snap_of_an_active_pool
    As an example of metadata snap usage for bz 286500

--- lib/dmtest/tests/thin-provisioning/tools_tests.rb
+++ lib/dmtest/tests/thin-provisioning/tools_tests.rb
@@ -62,6 +62,66 @@ class ToolsTests < ThinpTestCase
   define_test :you_can_dump_a_metadata_snapshot do
     allowed_on_live_metadata("thin_dump --metadata-snap #{@metadata_dev}")
   end
+
+  #--------------------------------
+
+  DUMP1 =<<EOF
+<superblock uuid="" time="0" transaction="0" data_block_size="128" nr_data_blocks="0">
+  <device dev_id="0" mapped_blocks="0" transaction="0" creation_time="0" snap_time="0">
+  </device>
+  <device dev_id="1" mapped_blocks="0" transaction="0" creation_time="0" snap_time="0">
+  </device>
+</superblock>
+EOF
+
+  DUMP2 =<<EOF
+<superblock uuid="" time="0" transaction="0" data_block_size="128" nr_data_blocks="0">
+  <device dev_id="0" mapped_blocks="16384" transaction="0" creation_time="0" snap_time="0">
+    <range_mapping origin_begin="0" data_begin="0" length="16384" time="0"/>
+  </device>
+  <device dev_id="1" mapped_blocks="0" transaction="0" creation_time="0" snap_time="0">
+  </device>
+</superblock>
+EOF
+
+  DUMP3 =<<EOF
+<superblock uuid="" time="0" transaction="0" data_block_size="128" nr_data_blocks="0">
+  <device dev_id="0" mapped_blocks="16384" transaction="0" creation_time="0" snap_time="0">
+    <range_mapping origin_begin="0" data_begin="0" length="16384" time="0"/>
+  </device>
+  <device dev_id="1" mapped_blocks="16384" transaction="0" creation_time="0" snap_time="0">
+    <range_mapping origin_begin="0" data_begin="16384" length="16384" time="0"/>
+  </device>
+</superblock>
+EOF
+
+  def check_metadata_snap(pool, txt)
+    metadata = nil
+    expected = StringIO.new(txt)
+
+    pool.message(0, "reserve_metadata_snap")
+    begin
+      Utils::with_temp_file('metadata_xml') do |file|
+        ProcessControl::run("thin_dump -m #{@metadata_dev} > #{file.path}")
+        file.rewind
+        assert FileUtils.compare_stream(file, expected)
+      end
+    ensure
+      pool.message(0, "release_metadata_snap")
+    end
+  end
+
+  define_test :thin_dump_a_metadata_snap_of_an_active_pool do
+    with_standard_pool(@size) do |pool|
+      with_new_thins(pool, @volume_size, 0, 1) do |thin, thin2|
+        check_metadata_snap(pool, DUMP1)
+        wipe_device(thin)
+        check_metadata_snap(pool, DUMP2)
+        wipe_device(thin2)
+        check_metadata_snap(pool, DUMP3)
+      end
+    end
+  end
 end
 
 #----------------------------------------------------------------
