c58827281df77a78b2b4a7b225a260df9ccd583c
Joe Thornber <ejt@redhat.com>
2017-05-04 10:33:07 +0100

    [cache] add an fio adaptability test

--- lib/dmtest/tests/cache/fio_tests.rb
+++ lib/dmtest/tests/cache/fio_tests.rb
@@ -61,6 +61,69 @@ class FIOTests < ThinpTestCase
   end
 
   define_tests_across(:origin_same_size_as_ssd, POLICY_NAMES)
+
+  #---------------------------------
+
+  def run_fio(dev, name)
+    outfile = AP("fio-#{name}.out")
+    cfgfile = LP("tests/cache/jharrigan.fio")
+
+    fs = FS::file_system(:ext4, dev)
+    fs.format(:discard => false)
+    fs.with_mount('./fio_test', :discard => false) do
+      Dir.chdir('./fio_test') do
+	ProcessControl.run("fio #{cfgfile} --output=#{outfile}")
+      end
+    end
+  end
+
+  define_test :fio_on_regions do
+    nr_sub_vols = 4
+    sub_vol_size = meg(1024 + 128)
+
+
+    stack = CacheStack.new(@dm, @metadata_dev, @data_dev,
+			   :policy => Policy.new('smq', :migration_threshold => 10240),
+			   :metadata_size => meg(128),
+			   :cache_size => sub_vol_size,
+			   :block_size => k(32),
+			   :data_size => sub_vol_size * nr_sub_vols)
+    stack.activate do |stack|
+      sub_vg = TinyVolumeManager::VM.new
+      sub_vg.add_allocation_volume(stack.cache)
+      nr_sub_vols.times do |n|
+        sub_vg.add_volume(linear_vol("vol#{n}", sub_vol_size))
+      end
+
+      nr_sub_vols.times do |n|
+	with_dev(sub_vg.table("vol#{n}")) do |vol|
+	  8.times do |iter|
+	    run_fio(vol, "vol#{n}-run#{iter}")
+	    pp CacheStatus.new(stack.cache)
+	  end
+	end
+      end
+    end
+  end
+
+  def baseline(dev, name)
+    sub_vol_size = meg(1024 + 128)
+
+    vg = TinyVolumeManager::VM.new
+    vg.add_allocation_volume(dev)
+    vg.add_volume(linear_vol("vol", sub_vol_size))
+    with_dev(vg.table("vol")) do |vol|
+      run_fio(vol, name)
+    end
+  end
+
+  define_test :fio_on_fast do
+    baseline(@metadata_dev, "fast")
+  end
+
+  define_test :fio_on_slow do
+    baseline(@data_dev, "slow")
+  end
 end
 
 #----------------------------------------------------------------
